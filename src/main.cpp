0bf44c50118c226d530d47ef50479564f8d36718
Joshua Lawrence Austill <jlaustill@gmail.com>
2024-03-17 17:37:58 -0600

    let's do this

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int minAppsOffIdle = 1000;
+int maxApps = 0;
+int appsValue = 0;
+int idleValue = 0;
+int appsThrottlePercentage = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(4, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  appsValue = analogRead(A0);
+  idleValue = digitalRead(4);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (appsValue < minAppsOffIdle && !idleValue) {
+    minAppsOffIdle = appsValue;
+  }
+
+  if (appsValue > maxApps) {
+    maxApps = appsValue;
+  }
+
+  appsThrottlePercentage = map(appsValue, 157, 815, 0, 100);
+
+  appsThrottlePercentage = appsThrottlePercentage < 0 ? 0 : appsThrottlePercentage;
+  appsThrottlePercentage = appsThrottlePercentage > 100 ? 100 : appsThrottlePercentage;
+
+  if (idleValue) {
+    appsThrottlePercentage = 0;
+  }
+  Serial.println("APPS MIN: " + (String)minAppsOffIdle + " MAX: " + (String)maxApps + " Throttle Percentage: " + (String)appsThrottlePercentage + " Idle: " + (String)idleValue);
 }
