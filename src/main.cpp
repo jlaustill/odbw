d108c41207c6f769cc0f1c99ff3b94b1cbb27ff5
Joshua Lawrence Austill <jlaustill@gmail.com>
2024-03-17 19:55:53 -0600

    We have a fully functioning drive by wire setup using the ford pedal, yay

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+Adafruit_MCP4725 dac;
 
 int minAppsOffIdle = 1000;
 int maxApps = 0;
 int appsValue = 0;
-int idleValue = 0;
+int isIdling = 0;
 int appsThrottlePercentage = 0;
+int appsPin = A0;
+int idlePin = 4;
+int relayPin = 5;
+
+int dacp3 = 0;
+int dac3p8 = 0;
+int dacOutput = 0;
 
 void setup() {
-  pinMode(4, INPUT_PULLUP);
+  pinMode(idlePin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
+  dac.begin(0x60);
+
+  dacp3 = map(.3, 0, 5, 0, 4095);
+  dac3p8 = map(3.8, 0, 5, 0, 4095);
 }
 
 void loop() {
-  appsValue = analogRead(A0);
-  idleValue = digitalRead(4);
+  appsValue = analogRead(appsPin);
+  isIdling = digitalRead(idlePin);
 
-  if (appsValue < minAppsOffIdle && !idleValue) {
+  if (appsValue < minAppsOffIdle && !isIdling) {
     minAppsOffIdle = appsValue;
   }
 
@@ -24,11 +40,22 @@ void loop() {
 
   appsThrottlePercentage = map(appsValue, 157, 815, 0, 100);
 
-  appsThrottlePercentage = appsThrottlePercentage < 0 ? 0 : appsThrottlePercentage;
-  appsThrottlePercentage = appsThrottlePercentage > 100 ? 100 : appsThrottlePercentage;
+  appsThrottlePercentage =
+      appsThrottlePercentage < 0 ? 0 : appsThrottlePercentage;
+  appsThrottlePercentage =
+      appsThrottlePercentage > 100 ? 100 : appsThrottlePercentage;
 
-  if (idleValue) {
+  if (isIdling) {
     appsThrottlePercentage = 0;
+    digitalWrite(relayPin, LOW);
+    dac.setVoltage(dacp3, false);
+  } else {
+    digitalWrite(relayPin, HIGH);
+    dacOutput = map(appsThrottlePercentage, 0, 100, dacp3, dac3p8);
+    dac.setVoltage(dacOutput, false);
   }
-  Serial.println("APPS MIN: " + (String)minAppsOffIdle + " MAX: " + (String)maxApps + " Throttle Percentage: " + (String)appsThrottlePercentage + " Idle: " + (String)idleValue);
+  
+  Serial.println("APPS MIN: " + (String)minAppsOffIdle +
+                 " MAX: " + (String)maxApps + " Throttle Percentage: " +
+                 (String)appsThrottlePercentage + " Idle: " + (String)isIdling);
 }
